0beb409c54d1efb98745b93f4e443d1c1810f685
DESKTOP-2JIU9FE\admin\Desktop\dev <hatgaji1240@gmail.com>
2024-07-02 17:26:58 +0900

    1. init

---
+++ everyseeing_web/src/main/java/com/es/web/EveryseeingWebApplication.java
@@ -0,0 +1,13 @@
+package com.es.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryseeingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryseeingWebApplication.class, args);
+	}
+
+}
