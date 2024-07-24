9b52ee7c5897975f280444c9ed1af4f0b049aad4
DESKTOP-2JIU9FE\admin\Desktop\dev <hatgaji1240@gmail.com>
2024-07-24 19:23:14 +0900

    1) 구글 이메일 연동 2) DB 정보 수정 3) 이메일 클래스 추가

---
+++ everyseeing_web/src/main/java/com/es/web/email/MailService.java
@@ -0,0 +1,38 @@
+package com.es.web.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	public void sendMail(MailData mailData) throws Exception{
+		
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+	
+		try {
+			String email = mailData.getEmail();
+			String title = mailData.getTitle();
+			String content = mailData.getContent();
+			
+			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(email); // 메일 수신자
+            mimeMessageHelper.setSubject(title); // 메일 제목
+            mimeMessageHelper.setText(content, true); // 메일 본문 내용, HTML 여부
+            javaMailSender.send(mimeMessage);
+            
+		} catch (MessagingException e) {
+			throw new RuntimeException (e);
+		}
+	}
+}
