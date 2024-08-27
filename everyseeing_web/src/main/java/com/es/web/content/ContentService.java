a060aba26f621dd2f2685659f032d487a82fdeb4
DESKTOP-2JIU9FE\admin\Desktop\dev <hatgaji1240@gmail.com>
2024-08-27 17:03:08 +0900

    1) 콘텐츠 화면 작업

--- everyseeing_web/src/main/java/com/es/web/content/ContentService.java
+++ everyseeing_web/src/main/java/com/es/web/content/ContentService.java
@@ -41,9 +41,35 @@ public class ContentService {
 	public Map<String, Object> getContentList(Map<String, Object> param) throws Exception {
 		ResponseMap respMap = new ResponseMap();
 
-		List<Map<String, Object>> list = contentMapper.getMovieContentList(param);
+		String menu = (String) param.get("menu");
+		List<Map<String, Object>> list = new ArrayList<>();
+		int total = 0;
+	
+		if(menu == null || menu.equals("")) {
+			menu = "A";
+		}
 		
-		List<List<Map<String, Object>>> data = new ArrayList<>();
+		if(menu.equals("A") || menu.equals("M") || menu.equals("T") || menu.equals("N")) {
+			// 일반 콘텐츠
+			list = contentMapper.getContentList(param);
+			total = contentMapper.getContentCnt(param);
+		} else if(menu.equals("B")) {
+			// 찜한 콘텐츠
+			list = contentMapper.getBookmarkContentList(param);
+			total = contentMapper.getBookmarkContentCnt(param);
+		}
+
+		// TV 콘텐츠 회차 매핑
+		for(Map<String, Object> content : list) {
+			String category = (String) content.get("category");
+			
+			if(category.equals("TV")) {
+				List<Map<String, Object>> roundList = contentMapper.getContentRoundList(content);
+				content.put("round_list", roundList);
+			}
+		}
+		
+		List<List<Map<String, Object>>> resultList = new ArrayList<>();
 		
 		// 5개씩 묶기
 		int limit = 5;
@@ -53,11 +79,11 @@ public class ContentService {
 				index = list.size();
 			}
 			List<Map<String, Object>> subList = list.subList(i, index);
-			data.add(subList);
+			resultList.add(subList);
 		}
 		
-		respMap.setBody("list", data);
-		respMap.setBody("total", contentMapper.getMovieContentCnt(param));
+		respMap.setBody("list", resultList);
+		respMap.setBody("total", total);
 		
 		return respMap.getResponseMap();
 	}
